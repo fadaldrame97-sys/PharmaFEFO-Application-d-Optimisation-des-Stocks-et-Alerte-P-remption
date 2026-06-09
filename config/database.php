6375a66a9cf0e5d218100721662bd0dd4e2c4c74
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-06-09 14:57:05 +0100

    Gestion erreur PDO

--- config/database.php
+++ config/database.php
@@ -6,6 +6,8 @@ class Database{
     public static function getConnection() : PDO {
         if(self::$pdo===null){
             self::$pdo=new PDO('mysql:host=localhost;dbname=PharmaFEFO','root','');
+
+            self::$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         }
         return self::$pdo;
 
