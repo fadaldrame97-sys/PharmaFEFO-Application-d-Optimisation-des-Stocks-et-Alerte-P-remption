3bcc290443b12c15755cfe3c6d44fb2db1dce60b
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-06-09 14:52:58 +0100

    Si le  est null on le crée

--- config/database.php
+++ config/database.php
@@ -4,6 +4,11 @@ class Database{
     private static? PDO $pdo=null;
 
     public static function getConnection() : PDO {
+        if(self::$pdo===null){
+            self::$pdo=new PDO('mysql:host=localhost;dbname=PharmaFEFO','root','');
+        }
+        return self::$pdo;
+
         
     }
 
