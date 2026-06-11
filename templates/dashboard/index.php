a7c454476ee9bcf6a6a2ab3aba237c660af4ca87
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-06-11 21:29:04 +0100

    Ajouter le style

--- templates/dashboard/index.php
+++ templates/dashboard/index.php
@@ -14,6 +14,9 @@ function getCriticityClass(string $criticity): string {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
+    <link rel="stylesheet" href="css.css">
 </head>
 <body>
     
