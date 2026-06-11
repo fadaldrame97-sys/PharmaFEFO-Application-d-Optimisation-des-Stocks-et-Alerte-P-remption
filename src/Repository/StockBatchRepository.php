bc131f382f892ac277015fc5003df8ac068a2bb6
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-06-11 11:23:36 +0100

    Appel les methodes getter dans exucute

--- src/Repository/StockBatchRepository.php
+++ src/Repository/StockBatchRepository.php
@@ -11,6 +11,12 @@ class StockBatchRepository
     public function create(StockBatch $stockBatch):bool{
         $query=" INSERT INTO stock_batches( product_id, lot_number,quantity,expiration_date,status)
                  VALUES (:product_id, :lot_number,:quantity,:expiration_date,:status)";
+        $statement=$this->pdo->prepare($query);
+        return $statement->execute(['product_id'=>$stockBatch->getProductId(),
+                                    'lot_number'=>$stockBatch->getLotNumber(),
+                                    'quantity'=>$stockBatch->getQuantity(),
+                                    'expriration_date'=>$stockBatch->getExpirationDate(), 
+                                    'status'=>$stockBatch->getStatus()]);                   
         
     }
 }
