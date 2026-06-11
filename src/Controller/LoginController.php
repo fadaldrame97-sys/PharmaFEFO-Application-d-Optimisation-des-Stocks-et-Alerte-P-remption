09b36ec69518eb4d6b3d9c32af7d3706c0e72afe
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-06-11 16:57:26 +0100

    Constucter et démmarage de session

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,4 +2,10 @@
 
 class LoginController{
     private UserRepository $userRepository;
+
+    public function __construct(UserRepository $userRepository){
+        $this->userRepository=$userRepository;
+        session_start();
+   }
 }
+
