e7b7b1ba21ff66c57b30b78feb6476dd2e895404
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-06-11 15:57:52 +0100

    function pour trouver un produit pas son id  et retour un objet de user

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -7,4 +7,18 @@ class ProductRepository
     {
         $this->pdo = Database::getConnection();
     }
+
+    public function findById(int $id): ?Product
+{
+    $query = "SELECT * FROM products WHERE id = :id";
+    $statement = $this->pdo->prepare($query);
+
+    if (!$statement) return null;
+
+    $statement->execute(['id' => $id]);
+    $product = $statement->fetchObject(Product::class);
+
+    return $product ?: null;
+}
+
 }
